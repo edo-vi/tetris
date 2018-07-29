950c8dd3a327bcdcdece30e015536da77d251cf1
Edoardo Z <edovi@icloud.com>
2018-07-29 15:06:33 +0200

    fixes wrong rendering process for lines 0 - 4

--- screen.c
+++ screen.c
@@ -11,16 +11,33 @@
 void draw_screen_tetris(struct screen_options *sco, struct screen_state *scs, struct blocks_state *bls) {
     map_active_state_to_board(scs, bls->active->normalized_pos);
     map_completed_blocks_to_board(scs,bls);
+    for (int i=0;i<3;i++) {
+        printf("  ");
+        for (int j=0; j<sco->board_dim_x;j++) {
+            if(*(scs->active_board+(i*sco->board_dim_x+j))==1 ) {
+                printf("\u204E");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
     printf("  ");
     for (int k=0;k<sco->board_dim_x;k++) {
-        printf("_");
+        if(*(scs->active_board+(3*sco->board_dim_x+k))==1 ) {
+            printf("\u204E");
+        } else {
+            printf("_");
+        }
+
     }
     printf("\n");
-    for (int i=0;i<sco->board_dim_y;i++) {
+
+    for (int i=4;i<sco->board_dim_y;i++) {
         printf("%s",sco->border);
         for (int j=0; j<sco->board_dim_x;j++) {
            if(*(scs->active_board+(i*sco->board_dim_x+j))==1 ) {
-               printf("*");
+               printf("\u204E");
            } else {
                printf(" ");
            }
@@ -36,29 +53,29 @@ void draw_screen_tetris(struct screen_options *sco, struct screen_state *scs, st
 void init_screen_tetris(struct screen_options* sco, struct screen_state* scs) {
     char dimension;
     int* board;
+    sco->board_dim_y+=4; // to let the blocks start outside and implement game loss logic
     do {
         printf("\nScegli dimensione dello schermo\n\n Piccolo     Normale     Grande\n    1           2          3\n");
         scanf(" %c",&dimension);
     } while(dimension<'1' ||dimension>'3');
     if (dimension=='1') {
         sco->board_dim_x=SMALL_X;
-        sco->board_dim_y=SMALL_Y;
+        sco->board_dim_y+=SMALL_Y;
         sco->dimension=1;
-        board=malloc(sizeof(int)*SMALL_Y*SMALL_X);
 
     } else if (dimension=='2') {
         sco->board_dim_x=MEDIUM_X;
-        sco->board_dim_y=MEDIUM_Y;
+        sco->board_dim_y+=MEDIUM_Y;
         sco->dimension=2;
-        board=malloc(sizeof(int)*MEDIUM_Y*MEDIUM_X);
+
 
     } else {
         sco->board_dim_x=LARGE_X;
-        sco->board_dim_y=LARGE_Y;
+        sco->board_dim_y+=LARGE_Y;
         sco->dimension=3;
-        board=malloc(sizeof(int)*LARGE_Y*LARGE_X);
 
     }
+    board=malloc(sizeof(int)*(sco->board_dim_y*sco->board_dim_x));
     sco->border="||";
     // init active_board
     scs->active_board=board;
